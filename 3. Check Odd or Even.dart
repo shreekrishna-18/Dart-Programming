78ea044c102a12326818cc6bd47ea3364bf46fec
Shreekrishna Yadav <165459334+shreekrishna-18@users.noreply.github.com>
2024-12-20 17:03:31 +0530

    Add files via upload

---
+++ 3. Check Odd or Even.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Define the number
+  int num = 11;
+
+  // Check if the number is even or odd
+  if (num % 2 == 0) {
+    print("The number is Even.");
+  } else {
+    print("The number is Odd.");
+  }
+}

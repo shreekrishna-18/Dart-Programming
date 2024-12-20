786920dc4fcf95ce09ac3e44427f2294425cf2ca
Shreekrishna Yadav <165459334+shreekrishna-18@users.noreply.github.com>
2024-12-20 17:07:04 +0530

    Add files via upload

---
+++ 6. Working with Lists.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Create a list of integers
+  List<int> numbers = [5, 3, 8, 1, 2];
+  
+  // Add the number 7 to the list
+  numbers.add(7);
+  
+  // Sort the list in ascending order
+  numbers.sort();
+  
+  // Print the sorted list
+  print("Sorted List: $numbers");
+}

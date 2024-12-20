2b79fc6bfbd616bdc37a4de4d368d8609762cde7
Shreekrishna Yadav <165459334+shreekrishna-18@users.noreply.github.com>
2024-12-20 17:01:26 +0530

    Add files via upload

---
+++ 2. simple_arthematic_opeeration.dart
@@ -0,0 +1,16 @@
+void main() {
+  int a = 10;
+  int b = 5;
+
+  // Sum operation
+  print('Sum: ${a + b}');
+  
+  // Difference operation
+  print('Difference: ${a - b}');
+  
+  // Product operation
+  print('Product: ${a * b}');
+  
+  // Quotient operation
+  print('Quotient: ${a / b}');
+}

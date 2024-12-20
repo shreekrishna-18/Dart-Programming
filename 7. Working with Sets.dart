e62a45e23f5da3155e6affc84357492cf457a1f9
Shreekrishna Yadav <165459334+shreekrishna-18@users.noreply.github.com>
2024-12-20 17:07:44 +0530

    Add files via upload

---
+++ 7. Working with Sets.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Create a set of unique integers
+  Set<int> numbers = {1, 2, 3, 4};
+  
+  // Add a new number 5 to the set
+  numbers.add(5);
+  
+  // Remove the number 2 from the set
+  numbers.remove(2);
+  
+  // Print the final set
+  print("Final Set: $numbers");
+}
